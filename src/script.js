257bb5e5e065407a1451d4de5a2c5386e6914dbe
JunDevl <juninhoplay.pedone@gmail.com>
2025-09-13 17:10:19 -0300

    Finished styling the webapp

--- src/script.js
+++ src/script.js
@@ -2,7 +2,10 @@ const libElem = document.querySelector(".library");
 const includeButton = document.querySelector("#include");
 
 libElem.addEventListener("click", (e) => {
+  console.log(e.target.getAttribute("class"));
+
   if (e.target.getAttribute("class") !== "trash") return;
+
   removeFromLibrary(e.target.getAttribute("uuid"));
 });
 
@@ -49,14 +52,29 @@ Book.prototype.toggleRead = function () { this.wasRead = !this.wasRead; };
 function insertToLibrary(...books) {
 
   const generateBookElement = (book) => {
-    const container = document.createElement("div");
-    container.className = "book";
-    container.setAttribute("uuid", book.uuid);
+    const container1 = document.createElement("div");
+    container1.className = "book";
+    container1.setAttribute("uuid", book.uuid);
 
+    const container2 = document.createElement("div");
+    container2.className = "book-container";
+
+    const aside = document.createElement("aside");
+    aside.className = "utils";
+
+    aside.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 24 24" class="trash" uuid="${book.uuid}">
+<path d="M 10 2 L 9 3 L 5 3 C 4.4 3 4 3.4 4 4 C 4 4.6 4.4 5 5 5 L 7 5 L 17 5 L 19 5 C 19.6 5 20 4.6 20 4 C 20 3.4 19.6 3 19 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 20 C 5 21.1 5.9 22 7 22 L 17 22 C 18.1 22 19 21.1 19 20 L 19 7 L 5 7 z M 9 9 C 9.6 9 10 9.4 10 10 L 10 19 C 10 19.6 9.6 20 9 20 C 8.4 20 8 19.6 8 19 L 8 10 C 8 9.4 8.4 9 9 9 z M 15 9 C 15.6 9 16 9.4 16 10 L 16 19 C 16 19.6 15.6 20 15 20 C 14.4 20 14 19.6 14 19 L 14 10 C 14 9.4 14.4 9 15 9 z"></path>
+</svg>`;
+
+    const readContainer = document.createElement("div");
+    readContainer.className = "read-container";
     const read = document.createElement("input");
     read.type = "checkbox";
     read.name = "wasRead";
     read.className = "read";
+    readContainer.append(read);
+
+    aside.appendChild(readContainer);
 
     const title = document.createElement("h2");
     title.className = "title";
@@ -66,24 +84,16 @@ function insertToLibrary(...books) {
     author.className = "author";
     author.textContent = book.author;
 
-    const footer = document.createElement("footer");
-
-    const pages = document.createElement("div");
+    const pages = document.createElement("p");
     pages.className = "pages";
-    pages.textContent = `${book.pages} pages`;
+    pages.textContent = `${book.pages.toLocaleString()} pages`;
 
-    const bin = document.createElement("div");
-    bin.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 72 72" class="trash" uuid="${book.uuid}">
-      <path 
-        d="M 32.5 9 C 28.364 9 25 12.364 25 16.5 L 25 18 L 17 18 C 14.791 18 13 19.791 13 22 C 13 24.209 14.791 26 17 26 L 17.232422 26 L 18.671875 51.916016 C 18.923875 56.449016 22.67875 60 27.21875 60 L 44.78125 60 C 49.32125 60 53.076125 56.449016 53.328125 51.916016 L 54.767578 26 L 55 26 C 57.209 26 59 24.209 59 22 C 59 19.791 57.209 18 55 18 L 47 18 L 47 16.5 C 47 12.364 43.636 9 39.5 9 L 32.5 9 z M 32.5 16 L 39.5 16 C 39.775 16 40 16.224 40 16.5 L 40 18 L 32 18 L 32 16.5 C 32 16.224 32.225 16 32.5 16 z M 36 28 C 37.104 28 38 28.896 38 30 L 38 47.923828 C 38 49.028828 37.104 49.923828 36 49.923828 C 34.896 49.923828 34 49.027828 34 47.923828 L 34 30 C 34 28.896 34.896 28 36 28 z M 27.392578 28.001953 C 28.459578 27.979953 29.421937 28.827641 29.460938 29.931641 L 30.085938 47.931641 C 30.123938 49.035641 29.258297 49.959047 28.154297 49.998047 C 28.131297 49.999047 28.108937 50 28.085938 50 C 27.012938 50 26.125891 49.148359 26.087891 48.068359 L 25.462891 30.068359 C 25.424891 28.964359 26.288578 28.040953 27.392578 28.001953 z M 44.607422 28.001953 C 45.711422 28.039953 46.575109 28.964359 46.537109 30.068359 L 45.912109 48.068359 C 45.874109 49.148359 44.986063 50 43.914062 50 C 43.891062 50 43.868703 49.999047 43.845703 49.998047 C 42.741703 49.960047 41.876063 49.035641 41.914062 47.931641 L 42.539062 29.931641 C 42.577062 28.827641 43.518422 27.979953 44.607422 28.001953 z">
-      </path>
-    </svg>`;
+    [title, author, pages].forEach((elem) => container2.appendChild(elem));
 
-    [pages, bin].forEach((elem) => footer.appendChild(elem));
-    [read, title, author, footer].forEach((elem) => container.appendChild(elem));
+    container1.appendChild(aside);
+    container1.appendChild(container2);
 
-    libElem.insertBefore(container, libElem.lastElementChild);
+    libElem.insertBefore(container1, libElem.lastElementChild);
   };
 
   for (book of books) {
@@ -114,18 +124,288 @@ function removeFromLibrary(...bookUUIDS) {
 
 insertToLibrary(
   {
-    title: "Vilma",
-    author: "Jun Pedia",
-    pages: 132
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
   },
   {
-    title: "Vann daime",
-    author: "Jun Pedia",
-    pages: 132
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
   },
   {
-    title: "Yuudoku Haiki-mono",
-    author: "Jun Pedia",
-    pages: 132
-  }
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
+  {
+    title: "A Song of Ice and Fire",
+    author: "George R. R. Martin",
+    pages: 4224
+  },
+  {
+    title: "Mushoku Tensei",
+    author: "Rifujin na Magonote",
+    pages: 7500
+  },
+  {
+    title: "Harry Potter",
+    author: "J. K. Rowling",
+    pages: 4100
+  },
 );
